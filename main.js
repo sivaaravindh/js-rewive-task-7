5b552be4f4ff433ccef981492b66041dbc9e56af
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-16 18:07:49 +0530

    index

---
+++ main.js
@@ -0,0 +1,15 @@
+let a=parseInt (prompt("Enter the value"));
+
+if (a % 2 === 0) {
+            console.log(a + " is even");   
+}
+else{
+        console.log(a+ " is odd number");
+} 
+
+
+
+
+
+
+
